1c980cf00af183acaf53f487875613ebe8cf2a0a
tajkiatoma <tajkiatoma@gmail.com>
2024-08-02 10:16:24 -0600

    Uncomment code

--- data_cleaner/random_discussion_cleaner.py
+++ data_cleaner/random_discussion_cleaner.py
@@ -6,13 +6,12 @@ from data_cleaner.main_statistics_provider import provide_discussion_filtering_s
 from data_cleaner.non_english_identifier import save_random_non_english_discussions
 
 if __name__ == '__main__':
-    # save_random_hidden_discussions()
-    # calculate_random_discussion_lengths()
-    # save_random_non_english_discussions()
+    save_random_hidden_discussions()
+    calculate_random_discussion_lengths()
+    save_random_non_english_discussions()
 
     all_random_discussions = get_all_random_discussions()
     cleaned_random_discussions = filter_discussions(all_random_discussions)
     save_selected_random_discussions(cleaned_random_discussions)
 
     provide_discussion_filtering_statistics(all_random_discussions)
-    
