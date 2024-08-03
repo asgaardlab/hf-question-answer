6613f1a467d8695fded0bd3add3de49dfcb0f5f6
tajkiatoma <tajkiatoma@gmail.com>
2024-08-03 03:01:02 -0600

    Remove order column from code

--- data_analyzer/random_discussion_selector.py
+++ data_analyzer/random_discussion_selector.py
@@ -2,13 +2,13 @@ from data_cleaner.discussion_reader import get_all_discussions
 from util import path
 
 
-def select_random_discussions():
+def select_random_discussions(number_of_discussions_to_select: int) -> None:
     all_discussions = get_all_discussions()
-    all_discussions.index = all_discussions.reset_index().index + 1
-    all_discussions.index.name = 'order'
-    all_discussions.sample(378).to_csv(path.ALL_RANDOM_DISCUSSIONS_FILE, index=True)
+    print(f'Selecting {number_of_discussions_to_select} random discussions from {len(all_discussions)} discussions')
+
+    all_discussions.sample(number_of_discussions_to_select).to_csv(path.ALL_RANDOM_DISCUSSIONS_FILE, index=False)
 
 
 if __name__ == '__main__':
-    select_random_discussions()
+    select_random_discussions(378)
     
