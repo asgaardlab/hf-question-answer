16cc7321a04fd98ba88c9093c783aa23da015aef
tajkiatoma <tajkiatoma@gmail.com>
2024-08-03 02:08:44 -0600

    Refactor

--- util/helper.py
+++ util/helper.py
@@ -6,6 +6,7 @@ import pandas as pd
 from data_collector.type.discussion import Discussion
 from util import path
 
+
 def get_model_dir_name(model_id):
     dir_name = model_id.replace('/', '@')
     if dir_name[-1] == ".":
@@ -17,14 +18,6 @@ def save_data_in_filepath(data: list | dict, file_path: Path) -> None:
     file_path.parent.mkdir(parents=True, exist_ok=True)
     with file_path.open('wb') as file:
         pickle.dump(data, file)
-
-
-def get_discussion_path(discussion_url: str) -> Path:
-    url_parts = discussion_url.split('/')
-    discussion_number = url_parts[-1]
-    model_id = url_parts[-4] + '/' + url_parts[-3]
-    model_dir_name = get_model_dir_name(model_id)
-    return path.DISCUSSIONS_DIRECTORY / model_dir_name / f'discussion_{discussion_number}.yaml'
 
 
 def get_discussion_url(discussion_path: str) -> str:
